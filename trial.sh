3e0476fcb383431f5c60225f49c0dc4b3077c339
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2021-11-17 01:10:15 +0530

    changing solver

--- trial.sh
+++ trial.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=115G
 #SBATCH -t 2-00:00:00
-#SBATCH --output=try.txt
+#SBATCH --output=try2.txt
 module add cuda/9.0
 module add cudnn/7-cuda-9.0
 source ~/keras/bin/activate
