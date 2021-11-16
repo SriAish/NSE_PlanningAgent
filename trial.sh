9d57e3391fc7a3b6341f3fd5d37e31b828657bee
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2021-11-17 03:29:59 +0530

    adding reward for end

--- trial.sh
+++ trial.sh
@@ -4,9 +4,9 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=115G
 #SBATCH -t 2-00:00:00
-#SBATCH --output=try_SCS_tight.txt
+#SBATCH --output=try_SCS_tight_reward.txt
 module add cuda/9.0
 module add cudnn/7-cuda-9.0
 source ~/keras/bin/activate
-echo "MOSEK"
+echo "SCS"
 python3.7 planningAgent.py
