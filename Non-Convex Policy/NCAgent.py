bb8cbebd3bd6376fef8dfbad08116ac90c8e7c93
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2021-11-26 00:58:59 +0530

    ch

--- Non-Convex Policy/NCAgent.py
+++ Non-Convex Policy/NCAgent.py
@@ -86,7 +86,7 @@ class DLPAgent:
             c = 0
             for a in actions:
                 c += e**self.pi[s][a]
-            if c != 0:
+            if actions:
                 self.m.Equation(c == su)
 
     def make_prob(self):
