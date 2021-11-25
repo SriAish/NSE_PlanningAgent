ff7778a96f48032cad9188848ca9c12a0f16309f
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2021-11-25 16:20:37 +0530

    ch

--- Planning Agent/readPolicy.py
+++ Planning Agent/readPolicy.py
@@ -27,18 +27,24 @@ class Policy:
             s = 0
             for a in self.policy[key]:
                 s += math.e**self.policy[key][a]
-            print(s)
+            # print(s)
         for key in self.x:
-            print(math.e**self.x[key])
-            print("s1: ",self.s1[key])
-            print("s2: ",self.s2[key])
+            if math.e**self.x[key] < 0:
+                print(math.e**self.x[key])
+            if self.s1[key] < 0:
+                print("s1: ",self.s1[key])
+            if self.s2[key] < 0:
+                print("s2: ",self.s2[key])
+            # print(math.e**self.x[key])
+            # print("s1: ",self.s1[key])
+            # print("s2: ",self.s2[key])
             # if s < 0.9:
                 # print(key)
                 # print(self.policy[key])
                 # print(s)
 
 if __name__ == '__main__':
-    agent = Policy('policy/Planning_Agent_Policy_3_3_3.pkl', 'policy/Planning_Agent_x_3_3_3.pkl', 'policy/Planning_Agent_s1_3_3_3.pkl', 'policy/Planning_Agent_s2_3_3_3.pkl')
+    agent = Policy('policy/Planning_Agent_Policy_3_3_9.pkl', 'policy/Planning_Agent_x_3_3_9.pkl', 'policy/Planning_Agent_s1_3_3_9.pkl', 'policy/Planning_Agent_s2_3_3_9.pkl')
     a = Actions()
     agent.getPi()
     # ((0, 0), (0, 0), False, 'p'), a.down
