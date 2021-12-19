c1b0390ef20fae63ed96fe864e1d86962c8c5049
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2021-12-20 00:17:48 +0530

    run for 3

--- Non-Convex Policy/vi_run.sh
+++ Non-Convex Policy/vi_run.sh
@@ -8,25 +8,19 @@
 module add cuda/9.0
 module add cudnn/7-cuda-9.0
 source ~/keras/bin/activate
-start=`date +%s`
-python3.7 NCAgent.py 3 0 0 1 1 2 2 3_3 1
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
-start=`date +%s`
-python3.7 NCAgent.py 3 0 0 1 1 2 2 3_3 2
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
 # start=`date +%s`
-# python3.7 NCAgent.py 3 0 0 1 1 2 2 3_3 3
+# python3.7 NCAgent.py 3 0 0 1 1 2 2 3_3 1
 # end=`date +%s`
 # runtime=$((end-start))
 # echo $runtime
 # echo "--------------------"
-# start=`date +%s`
+start=`date +%s`
+python3.7 NCAgent.py 3 0 0 1 1 2 2 3_3 3
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
 # python3.7 NCAgent.py 5 1 1 2 2 5 5 5_5
 # end=`date +%s`
 # runtime=$((end-start))
