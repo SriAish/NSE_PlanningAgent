4210374eb8812d91e9a083d20c8a3bca585b5546
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2021-12-21 03:38:35 +0530

    run simulations

--- simulations.sh
+++ simulations.sh
@@ -4,14 +4,17 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=100G
 #SBATCH -t 2-00:00:00
-#SBATCH --output=temp
+#SBATCH --output=temp.txt
 module add cuda/9.0
 module add cudnn/7-cuda-9.0
 source ~/keras/bin/activate
 python3.7 PolicySimulation.py VI/policy_values/VIp_3_3.pkl
 python3.7 PolicySimulation.py 'Convex Policy e_version/policy/C_Agent_Policy_ni_3_3_3.pkl'
 python3.7 PolicySimulation.py 'Convex Policy e_version/policy/C_Agent_Policy_ni_1_3_3.pkl'
-python3.7 PolicySimulation.py 'Non-Convex Policy e_version/policy/C_Agent_Policy_ni_1_3_3.pkl'
-python3.7 PolicySimulation.py 'Non-Convex Policy e_version/policy/C_Agent_Policy_ni_1_3_3.pkl'
-python3.7 PolicySimulation.py 'Non-Convex Policy/policy/C_Agent_Policy_nor_1_3_3.pkl'
-python3.7 PolicySimulation.py 'Non-Convex Policy/policy/C_Agent_Policy_nor_3_3_3.pkl'
+python3.7 PolicySimulation.py 'Non-Convex Policy e_version/policy/NC_Agent_Policy_ni_1_3_3.pkl'
+python3.7 PolicySimulation.py 'Non-Convex Policy e_version/policy/NC_Agent_Policy_ni_1_3_3.pkl'
+python3.7 PolicySimulation.py 'Non-Convex Policy/policy/NC_Agent_Policy_nor_1_3_3.pkl'
+python3.7 PolicySimulation.py 'Non-Convex Policy/policy/NC_Agent_Policy_nor_3_3_3.pkl'
+python3.7 PolicySimulation.py 'Non-Convex Policy/policy/NC_Agent_Policy_no_upper_3_31.pkl'
+python3.7 PolicySimulation.py 'Non-Convex Policy/policy/NC_Agent_Policy_no_upper_3_32.pkl'
+python3.7 PolicySimulation.py 'Non-Convex Policy/policy/NC_Agent_Policy_no_upper_3_33.pkl'
