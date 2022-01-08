49e1fdefc7da989a41597a5d0ba8422b37d16373
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-01-08 22:26:52 +0530

    find x

--- Find x/NCAgent.py
+++ Find x/NCAgent.py
@@ -107,8 +107,8 @@ class NCAgent:
         sys.stdout.flush()
         self.make_constraints_eqn1()
         print("eq1")
-        self.make_constraints_eqn2()
-        print("eq2")
+        # self.make_constraints_eqn2()
+        # print("eq2")
         sys.stdout.flush()
 
     def calculate_pi(self):
