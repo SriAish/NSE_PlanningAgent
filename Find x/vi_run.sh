863327f23ff1b0075ee9ee425048432cf0b90195
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-01-08 22:23:50 +0530

    find x

--- Find x/vi_run.sh
+++ Find x/vi_run.sh
@@ -14,23 +14,23 @@ source ~/keras/bin/activate
 # runtime=$((end-start))
 # echo $runtime
 # echo "--------------------"
-# start=`date +%s`
-# python3.7 NCAgent.py 3 0 0 1 1 2 2 3_3 3
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
 start=`date +%s`
-python3.7 NCAgent.py 5 1 1 2 2 4 4 5_5 1
+python3.7 NCAgent.py 3 0 0 1 1 2 2 3_3 3
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
-start=`date +%s`
-python3.7 NCAgent.py 5 1 1 2 2 4 4 5_5 3
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
+# start=`date +%s`
+# python3.7 NCAgent.py 5 1 1 2 2 4 4 5_5 1
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
+# echo "--------------------"
+# start=`date +%s`
+# python3.7 NCAgent.py 5 1 1 2 2 4 4 5_5 3
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
 # echo "--------------------"
 # start=`date +%s`
 # python3.7 NCAgent.py 7 3 3 2 2 3 6 7_7 1
