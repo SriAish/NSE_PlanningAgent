12a6177ffd5cd0f505ebb285b30a2b8f46d17321
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-02-01 00:43:22 +0530

    original env

--- Non-Convex Policy - Baseline/vi_run.sh
+++ Non-Convex Policy - Baseline/vi_run.sh
@@ -39,7 +39,7 @@ source ~/keras/bin/activate
 # echo $runtime
 # echo "--------------------"
 start=`date +%s`
-python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_og 3
+python3.7 NCAgent.py 7 3 3 2 2 3 6 7_7_og 3
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
