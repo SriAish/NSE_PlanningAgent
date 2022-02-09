11adbe69cee2be573ad5d4027f7f18995961541e
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-02-10 00:26:14 +0530

    check changes

--- Non-Convex Policy - Baseline/actions.py
+++ Non-Convex Policy - Baseline/actions.py
@@ -9,9 +9,9 @@ class Actions:
         self.drop = "drop"
         # self.allActions = [self.down, self.up, self.right, self.left, self.pick_up, self.wrap]
         # self.allActions = [self.down, self.up, self.right, self.left, self.pick_up, self.drop, self.wrap]
-        self.allActions = [self.down, self.up, self.right, self.left, self.pick_up]
-        self.boxActions = [self.pick_up]
-        # self.boxActions = [self.pick_up, self.drop]
+        self.allActions = [self.down, self.up, self.right, self.left, self.pick_up, self.drop]
+        # self.boxActions = [self.pick_up]
+        self.boxActions = [self.pick_up, self.drop]
         self.wrapActions = [self.wrap]
         self.moveActions = [self.down, self.up, self.right, self.left]
 
