7e4881acfc6db210d8237db9ef0810f5014799a0
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-02-25 01:28:10 +0530

    r

--- Non-Convex Policy - Baseline/vi_run_p4.sh
+++ Non-Convex Policy - Baseline/vi_run_p4.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=40G
 #SBATCH -t 2-00:00:00
-#SBATCH --output=resul4.txt
+#SBATCH --output=resul3.txt
 module add cuda/9.0
 module add cudnn/7-cuda-9.0
 source ~/keras/bin/activate
@@ -43,24 +43,24 @@ source ~/keras/bin/activate
 # end=`date +%s`
 # runtime=$((end-start))
 # echo $runtime
-echo "--------------------"
-start=`date +%s`
-python3.7 NCAgent4.py 7 3 3 2 2 3 6 7_7_og_63 3 6 3
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
-start=`date +%s`
-python3.7 NCAgent4.py 7 3 3 2 2 3 6 7_7_og_03 3 0 3
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
-start=`date +%s`
-python3.7 NCAgent4.py 7 3 3 2 2 3 6 7_7_og_12 3 1 2
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
+# echo "--------------------"
+# start=`date +%s`
+# python3.7 NCAgent4.py 7 3 3 2 2 3 6 7_7_og_63 3 6 3
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
+# echo "--------------------"
+# start=`date +%s`
+# python3.7 NCAgent4.py 7 3 3 2 2 3 6 7_7_og_03 3 0 3
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
+# echo "--------------------"
+# start=`date +%s`
+# python3.7 NCAgent4.py 7 3 3 2 2 3 6 7_7_og_12 3 1 2
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
 echo "--------------------"
 start=`date +%s`
 python3.7 NCAgent4.py 7 3 3 2 2 3 6 7_7_og_30 3 3 0
