db5a949ad18e9c3a3202d65bef35cdb97b43b981
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-02-25 02:34:05 +0530

    r

--- Non-Convex Policy/vi_run.sh
+++ Non-Convex Policy/vi_run.sh
@@ -39,31 +39,31 @@ source ~/keras/bin/activate
 # echo $runtime
 echo "--------------------"
 start=`date +%s`
-python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE 3 5 4 0.1
+python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE_1 3 5 4 0.1
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
 start=`date +%s`
-python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE 3 5 4 0.2
+python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE_2 3 5 4 0.2
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
 start=`date +%s`
-python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE 3 5 4 0.3
+python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE_3 3 5 4 0.3
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
 start=`date +%s`
-python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE 3 5 4 0.4
+python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE_4 3 5 4 0.4
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
 start=`date +%s`
-python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE 3 5 4 0.5
+python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE_5 3 5 4 0.5
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
