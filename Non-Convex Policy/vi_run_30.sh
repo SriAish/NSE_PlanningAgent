3ecda0ec93d0d494670cdda6deb4a8263dbe657d
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-02-25 06:44:03 +0530

    r

--- Non-Convex Policy/vi_run_30.sh
+++ Non-Convex Policy/vi_run_30.sh
@@ -38,9 +38,9 @@ echo $runtime
 # end=`date +%s`
 # runtime=$((end-start))
 # echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE_30_5 3 3 0 0.5 7.79 30
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 NCAgent_2.py 7 3 3 2 2 3 6 7_7_NSE_30_5 3 3 0 0.5 7.79 30
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
