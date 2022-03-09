60d156ac7c73c8dba8a6b48dd85c055327961ac0
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-03-09 18:47:00 +0530

    ne agent files

---
+++ Agent/run_agent.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A research
+#SBATCH --cpus-per-gpu=2
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=40G
+#SBATCH -t 2-00:00:00
+#SBATCH --output=resul3.txt
+module add cuda/9.0
+module add cudnn/7-cuda-9.0
+source ~/keras/bin/activate
+
+start=`date +%s`
+python3.7 NCAgent3.py 3 1 1 1 1 2 2 3_3_ogt 3
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
