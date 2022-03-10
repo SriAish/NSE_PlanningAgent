35f70805befafb510aa0354e9aa4a31473b11fdf
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-03-10 21:47:15 +0530

    7x7

--- Agent/run_agent_FSA_constrained.sh
+++ Agent/run_agent_FSA_constrained.sh
@@ -2,7 +2,7 @@
 #SBATCH -A research
 #SBATCH --cpus-per-gpu=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-cpu=120G
+#SBATCH --mem-per-cpu=100G
 #SBATCH -t 2-00:00:00
 #SBATCH --output=resul3.txt
 module add cuda/9.0
