15c1950287bc08d3916430b3af30af0dd528cf33
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-03-17 23:08:36 +0530

    ch

--- Agent/NCAgent3.py
+++ Agent/NCAgent3.py
@@ -159,7 +159,7 @@ if __name__ == '__main__':
     #     e_state.append((g_pos, g_pos, True, False, 'p', i))
     #     e_state.append((g_pos, g_pos, True, True, 'p', i))
     # print(e_state)
-    BP = BoxPushingConstants(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), (int(sys.argv[4]), int(sys.argv[5])), g_pos, (1, 1))
+    BP = BoxPushingConstants(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), (int(sys.argv[4]), int(sys.argv[5])), g_pos, (3, 0))
     
     # locations = [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 1), (2, 5), (3, 1), (3, 5), (4, 1), (4, 5), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5)]
     # locations = [(3, 0), (1, 2), (0, 3), (6, 3), (5, 4)]
