90bbfac5335f9921c8b2144b9106ce115fd75db0
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-03-23 13:57:14 +0530

    result

--- Agent/run_agent.sh
+++ Agent/run_agent.sh
@@ -9,18 +9,18 @@ module add cuda/9.0
 module add cudnn/7-cuda-9.0
 source ~/keras/bin/activate
 
-start=`date +%s`
-python3.7 NCAgent3.py 3 1 1 1 1 2 2 3_3_ogt 3
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
 # start=`date +%s`
-# python3.7 NCAgent3.py 7 3 3 2 2 3 6 7_7_ogt 3
+# python3.7 NCAgent3.py 3 1 1 1 1 2 2 3_3_ogt 3
 # end=`date +%s`
 # runtime=$((end-start))
 # echo $runtime
 # echo "--------------------"
+start=`date +%s`
+python3.7 NCAgent3.py 7 3 3 2 2 3 6 7_7_ogt 3
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
 # start=`date +%s`
 # python3.7 NCAgent3.py 15 7 3 6 4 7 14 15_15_ogt 3
 # end=`date +%s`
