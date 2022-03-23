18d99a0b013e4860222db86697d5323dcb6210b9
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-03-23 10:29:25 +0530

    result

--- Agent/run_agent_FSA.sh
+++ Agent/run_agent_FSA.sh
@@ -16,67 +16,73 @@ source ~/keras/bin/activate
 # echo $runtime
 echo "--------------------"
 start=`date +%s`
-python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 1 > 7_10
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 10 > 7_100.txt
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.9 > 7_9
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.8 > 7_8
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.7 > 7_7
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.6 > 7_6
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.5 > 7_5
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.4 > 7_4
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.3 > 7_3
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.2 > 7_2
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.1 > 7_1
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0 > 7_0
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 1 > 7_10.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.9 > 7_9.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.8 > 7_8.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.7 > 7_7.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.6 > 7_6.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.5 > 7_5.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.4 > 7_4.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.3 > 7_3.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.2 > 7_2.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.1 > 7_1.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0 > 7_0.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
