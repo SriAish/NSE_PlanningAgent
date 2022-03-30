9fc568e424b8f64181a9ce14dac1c450c543ee0a
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-03-31 02:03:29 +0530

    result

--- Agent/run_agent_FSA.sh
+++ Agent/run_agent_FSA.sh
@@ -17,15 +17,3 @@ end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
-start=`date +%s`
-python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.0001 0.0001 > sm_7_0001.txt
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
-start=`date +%s`
-python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.00001 0.00001 > sm_7_00001.txt
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
