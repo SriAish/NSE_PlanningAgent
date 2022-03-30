16b11a6de0765b5f3e7507ef2edea08e8650b204
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-03-31 01:24:22 +0530

    result

--- Agent/run_agent_FSA.sh
+++ Agent/run_agent_FSA.sh
@@ -12,7 +12,13 @@ source ~/keras/bin/activate
 
 
 start=`date +%s`
-python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 1 -1 0 > m_7_0.txt
+python3.7 FSA_Agent_2.py 7 3 3 2 2 3 6 7_7 1 0.1 0.1 > nsm_7_1.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+python3.7 FSA_Agent_2.py 7 3 3 2 2 3 6 7_7 1 0.4 0.4 > nsm_7_4.txt
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
