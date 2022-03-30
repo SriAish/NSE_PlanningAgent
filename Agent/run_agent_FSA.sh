be8c3a2ca32cd9f11f42b489180f4c78525ab921
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-03-31 00:07:41 +0530

    result

--- Agent/run_agent_FSA.sh
+++ Agent/run_agent_FSA.sh
@@ -9,14 +9,15 @@ module add cuda/9.0
 module add cudnn/7-cuda-9.0
 source ~/keras/bin/activate
 
+
 start=`date +%s`
-python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 -1 0.01 > m2_7_01.txt
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0 -1 > 7_0.txt
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
 start=`date +%s`
-python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.4 0.4 > sm_7_4.txt
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.1 -1 > 7_1.txt
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
@@ -27,15 +28,3 @@ end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0 -1 > 7_0.txt
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.1 -1 > 7_1.txt
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
