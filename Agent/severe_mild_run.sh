2463e3d2ef16e99ecea6dbb76eff19571d60bd23
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-04-05 17:02:05 +0530

    result

--- Agent/severe_mild_run.sh
+++ Agent/severe_mild_run.sh
@@ -9,30 +9,30 @@ module add cuda/9.0
 module add cudnn/7-cuda-9.0
 source ~/keras/bin/activate
 
-# start=`date +%s`
-# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 1 0 0 > sm_1_7_0.txt
-# end=`date +%s`
-# runtime=$((end-start))
-# echo $runtime
-# echo "--------------------"
 start=`date +%s`
-python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 1 0.1 0.1 > sm_1_7_1.txt
+python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 1 0 0 > sm_1_7_0.txt
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
-start=`date +%s`
+# start=`date +%s`
+# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 1 0.1 0.1 > sm_1_7_1.txt
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
+# echo "--------------------"
+# start=`date +%s`
 python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 1 0.2 0.2 > sm_1_7_2.txt
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
-start=`date +%s`
-python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 1 0.3 0.3 > sm_1_7_3.txt
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
+# start=`date +%s`
+# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 1 0.3 0.3 > sm_1_7_3.txt
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
+# echo "--------------------"
 start=`date +%s`
 python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 1 0.4 0.4 > sm_1_7_4.txt
 end=`date +%s`
