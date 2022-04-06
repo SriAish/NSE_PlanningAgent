e619276727c58b8b5992e5161f5fb11e1b07bc19
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-04-07 01:05:39 +0530

    result

--- Agent/severe_mild_run_3.sh
+++ Agent/severe_mild_run_3.sh
@@ -34,7 +34,7 @@ source ~/keras/bin/activate
 # echo $runtime
 # echo "--------------------"
 start=`date +%s`
-python3.7 FSA_Agent_2.py 7 3 3 2 2 3 6 7_7 3 0.1 0.1 > sm_3_7_0.txt
+python3.7 FSA_Agent_2.py 7 3 3 2 2 3 6 7_7 3 0 0 > sm_3_7_0.txt
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
@@ -51,18 +51,18 @@ echo "--------------------"
 # runtime=$((end-start))
 # echo $runtime
 # echo "--------------------"
-start=`date +%s`
-python3.7 FSA_Agent_2.py 7 3 3 2 2 3 6 7_7 3 0.2 0.2 > sm_3_7_2.txt
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
-start=`date +%s`
-python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.2 0.2 > osm_3_7_2.txt
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
+# start=`date +%s`
+# python3.7 FSA_Agent_2.py 7 3 3 2 2 3 6 7_7 3 0.2 0.2 > sm_3_7_2.txt
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
+# echo "--------------------"
+# start=`date +%s`
+# python3.7 FSA_Agent.py 7 3 3 2 2 3 6 7_7 3 0.2 0.2 > osm_3_7_2.txt
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
+# echo "--------------------"
 # start=`date +%s`
 # python3.7 FSA_Agent_2.py 7 3 3 2 2 3 6 7_7 3 0.3 0.3 > sm_3_7_3.txt
 # end=`date +%s`
