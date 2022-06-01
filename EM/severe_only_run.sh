cc3f938d2d339fe5e3ee18b7e1b32c98b485a141
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-06-01 12:07:12 +0530

    result

--- EM/severe_only_run.sh
+++ EM/severe_only_run.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=80G
 #SBATCH -t 2-00:00:00
-#SBATCH --output=result2.txt
+#SBATCH --output=result.txt
 module add cuda/11.6
 module add cudnn/8.4.0-cuda-11.6
 source ~/keras/bin/activate
