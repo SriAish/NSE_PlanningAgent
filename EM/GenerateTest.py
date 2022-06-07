abee3578b92c282ebe689177a18de9c935d06ac4
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-06-07 16:55:40 +0530

    result

--- EM/GenerateTest.py
+++ EM/GenerateTest.py
@@ -71,7 +71,7 @@ def save(name, t):
     file_to_write = open(name, "wb")
     pickle.dump(t, file_to_write)
 
-R = generate_trajectories(2000)
+R = generate_trajectories(1000)
 print(random.sample(list(R), 10), len(R))
 random.shuffle(R)
 n = round(0.8*len(R))
