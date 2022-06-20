f006c493c768f73f22ce409e9f78967c6a0aca9f
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-06-20 23:59:55 +0530

    result

--- VI/VIAgent.py
+++ VI/VIAgent.py
@@ -18,7 +18,7 @@ class VIAgent:
         self.initializeStateValues()
         self.gamma = gamma  
         self.delta = delta
-        self.locations = [(sys.argv[10], sys.argv[11])]
+        self.locations = [(int(sys.argv[10]), int(sys.argv[11]))]
         self.init_belief()
 
     def init_belief(self):
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     e_state.append((g_pos, g_pos, True, False, 'p'))
     e_state.append((g_pos, g_pos, True, True, 'p'))
     print(e_state)
-    BP = BoxPushingConstants(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), (int(sys.argv[4]), int(sys.argv[5])), e_state, (sys.argv[10], sys.argv[11]))
+    BP = BoxPushingConstants(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), (int(sys.argv[4]), int(sys.argv[5])), e_state, (int(sys.argv[10]), int(sys.argv[11])))
     agent = VIAgent(BP, delta=0.001)
     policy = agent.generatePolicy()
     print(policy)
