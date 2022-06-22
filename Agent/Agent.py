031915684ba210db250b7e1e1b4538d468676127
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-06-23 02:44:45 +0530

    result

--- Agent/Agent.py
+++ Agent/Agent.py
@@ -1,5 +1,5 @@
 from EnvConst import BoxPushingConstants
-from FSAConst import FSAConstants
+from FSAConst2 import FSAConstants
 import sys
 import pickle
 import itertools
@@ -36,7 +36,7 @@ class FSAgent:
         self.belief_state = []
         for i in self.locations:
             s = (init_loc, i, False, False, 'p')
-            self.belief_state.append((self.FSA.(s), s))
+            self.belief_state.append((self.FSA.nextState(s), s))
 
     def init_var(self):
         self.x = {}
