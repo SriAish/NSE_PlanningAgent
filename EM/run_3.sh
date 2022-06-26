4ca8e363dabebf3dc49ee4a4dc9c231e971eb367
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-06-26 22:23:25 +0530

    result

--- EM/run_3.sh
+++ EM/run_3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -A research
+#SBATCH -A MLL
 #SBATCH --cpus-per-gpu=2
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=80G
@@ -9,25 +9,25 @@ module add cuda/11.6
 module add cudnn/8.4.0-cuda-11.6
 source ~/keras/bin/activate
 
-python3 EM3.py data_set/BP_15_15_10 7
-python3 test3.py data_set/BP_15_15_10 7
-echo "--------------------"
-python3 EM3.py data_set/BP_15_15_10 8
-python3 test3.py data_set/BP_15_15_10 8
-echo "--------------------"
-python3 EM3.py data_set/BP_15_15_10 9
-python3 test3.py data_set/BP_15_15_10 9
-echo "--------------------"
-echo "--------------------"
-echo "--------------------"
-python3 EM3.py data_set/BP_15_15_20 7
-python3 test3.py data_set/BP_15_15_20 7
-echo "--------------------"
-python3 EM3.py data_set/BP_15_15_20 8
-python3 test3.py data_set/BP_15_15_20 8
-echo "--------------------"
-python3 EM3.py data_set/BP_15_15_20 9
-python3 test3.py data_set/BP_15_15_20 9
+# python3 EM3.py data_set/BP_15_15_10 7
+# python3 test3.py data_set/BP_15_15_10 7
+# echo "--------------------"
+# python3 EM3.py data_set/BP_15_15_10 8
+# python3 test3.py data_set/BP_15_15_10 8
+# echo "--------------------"
+# python3 EM3.py data_set/BP_15_15_10 9
+# python3 test3.py data_set/BP_15_15_10 9
+# echo "--------------------"
+# echo "--------------------"
+# echo "--------------------"
+# python3 EM3.py data_set/BP_15_15_20 7
+# python3 test3.py data_set/BP_15_15_20 7
+# echo "--------------------"
+# python3 EM3.py data_set/BP_15_15_20 8
+# python3 test3.py data_set/BP_15_15_20 8
+# echo "--------------------"
+# python3 EM3.py data_set/BP_15_15_20 9
+# python3 test3.py data_set/BP_15_15_20 9
 echo "--------------------"
 echo "--------------------"
 echo "--------------------"
