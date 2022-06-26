d318e497914f50e4347148e65facf4535af42508
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-06-26 22:20:04 +0530

    result

--- Whole/EM.py
+++ Whole/EM.py
@@ -216,7 +216,7 @@ out_sym = ['N', 'S', 'M', 3]
 file_name = sys.argv[1][sys.argv[1].index("/")+1:]
 
 all_start_time = time.time()
-for i_try in range(2):
+for i_try in range(10):
     print(sys.argv[1])
     print("states: ", sys.argv[2], "trial: ", i_try)
     start_time = time.time()
--- Whole/run.sh
+++ Whole/run.sh
@@ -23,5 +23,17 @@ python3 EM.py data_set/BP_n_30 5
 echo "Checking Accuracy of learned FSAs"
 python3 test.py data_set/BP_n_30 5
 echo "Create FSA in the expected format"
-python3 makeFSA.py data_set/BP_n_10 5
+python3 makeFSA.py data_set/BP_n_30 5
 echo "running agent"
+python3.7 FSA_Agent_combVar.py 7 3 3 2 2 3 6 7_7_0_3_0 1 0 0 data_set/BP_n_30 5 > LP_sm_1_7_0.txt
+echo "--------------------"
+echo "--------------------"
+echo "--------------------"
+echo "Start learning FSA"
+python3 EM.py data_set/BP_n_40 5
+echo "Checking Accuracy of learned FSAs"
+python3 test.py data_set/BP_n_40 5
+echo "Create FSA in the expected format"
+python3 makeFSA.py data_set/BP_n_40 5
+echo "running agent"
+python3.7 FSA_Agent_combVar.py 7 3 3 2 2 3 6 7_7_0_3_0 1 0 0 data_set/BP_n_40 5 > LP_sm_1_7_0.txt
