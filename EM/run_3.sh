dd37fa5d2b2af306e7aa21f9a7b94e9add49e2c2
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-06-29 11:30:15 +0530

    result

--- EM/run_3.sh
+++ EM/run_3.sh
@@ -4,13 +4,13 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=80G
 #SBATCH -t 2-00:00:00
-#SBATCH --output=result4.txt
+#SBATCH --output=result6.txt
 module add cuda/11.6
 module add cudnn/8.4.0-cuda-11.6
 source ~/keras/bin/activate
 
-python3 EM3.py data_set/BP_15_15_35_13 7
-python3 test3.py data_set/BP_15_15_35_13 7
+# python3 EM3.py data_set/BP_15_15_35_13 7
+# python3 test3.py data_set/BP_15_15_35_13 7
 echo "--------------------"
 python3 EM3.py data_set/BP_15_15_35_13 8
 python3 test3.py data_set/BP_15_15_35_13 8
