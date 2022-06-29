0491b9e56bfd9ea27f602577c221afcce789dcfb
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-06-29 22:39:28 +0530

    result

--- EM/1.sh
+++ EM/1.sh
@@ -1,6 +1,6 @@
-python3 EM4.py data_set/BP_15_15_40_13 8
+python3 EM3.py data_set/BP_15_15_40_13 8
 python3 test4.py data_set/BP_15_15_40_13 8
 echo "--------------------"
-python3 EM4.py data_set/BP_15_15_40_13 9
+python3 EM3.py data_set/BP_15_15_40_13 9
 python3 test4.py data_set/BP_15_15_40_13 9
 echo "--------------------"
