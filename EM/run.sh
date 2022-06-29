7afac52694e3feb7f60760b1a77bcaec9969640d
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-06-29 22:42:29 +0530

    result

--- EM/run.sh
+++ EM/run.sh
@@ -9,113 +9,113 @@ module add cuda/11.6
 module add cudnn/8.4.0-cuda-11.6
 source ~/keras/bin/activate
 
-python3.5 EM2.py data_set/BP_wh_10 4
-python3.5 test2.py data_set/BP_wh_10 4
+python3 EM2.py data_set/BP_wh_10 4
+python3 test2.py data_set/BP_wh_10 4
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_10 5
-python3.5 test2.py data_set/BP_wh_10 5
+python3 EM2.py data_set/BP_wh_10 5
+python3 test2.py data_set/BP_wh_10 5
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_10 6
-python3.5 test2.py data_set/BP_wh_10 6
-echo "--------------------"
-echo "--------------------"
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_20 4
-python3.5 test2.py data_set/BP_wh_20 4
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_20 5
-python3.5 test2.py data_set/BP_wh_20 5
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_20 6
-python3.5 test2.py data_set/BP_wh_20 6
+python3 EM2.py data_set/BP_wh_10 6
+python3 test2.py data_set/BP_wh_10 6
 echo "--------------------"
 echo "--------------------"
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_30 4
-python3.5 test2.py data_set/BP_wh_30 4
+python3 EM2.py data_set/BP_wh_20 4
+python3 test2.py data_set/BP_wh_20 4
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_30 5
-python3.5 test2.py data_set/BP_wh_30 5
+python3 EM2.py data_set/BP_wh_20 5
+python3 test2.py data_set/BP_wh_20 5
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_30 6
-python3.5 test2.py data_set/BP_wh_30 6
-echo "--------------------"
-echo "--------------------"
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_40 4
-python3.5 test2.py data_set/BP_wh_40 4
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_40 5
-python3.5 test2.py data_set/BP_wh_40 5
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_40 6
-python3.5 test2.py data_set/BP_wh_40 6
+python3 EM2.py data_set/BP_wh_20 6
+python3 test2.py data_set/BP_wh_20 6
 echo "--------------------"
 echo "--------------------"
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_50 4
-python3.5 test2.py data_set/BP_wh_50 4
+python3 EM2.py data_set/BP_wh_30 4
+python3 test2.py data_set/BP_wh_30 4
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_50 5
-python3.5 test2.py data_set/BP_wh_50 5
+python3 EM2.py data_set/BP_wh_30 5
+python3 test2.py data_set/BP_wh_30 5
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_50 6
-python3.5 test2.py data_set/BP_wh_50 6
-echo "--------------------"
-echo "--------------------"
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_60 4
-python3.5 test2.py data_set/BP_wh_60 4
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_60 5
-python3.5 test2.py data_set/BP_wh_60 5
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_60 6
-python3.5 test2.py data_set/BP_wh_60 6
+python3 EM2.py data_set/BP_wh_30 6
+python3 test2.py data_set/BP_wh_30 6
 echo "--------------------"
 echo "--------------------"
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_70 4
-python3.5 test2.py data_set/BP_wh_70 4
+python3 EM2.py data_set/BP_wh_40 4
+python3 test2.py data_set/BP_wh_40 4
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_70 5
-python3.5 test2.py data_set/BP_wh_70 5
+python3 EM2.py data_set/BP_wh_40 5
+python3 test2.py data_set/BP_wh_40 5
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_70 6
-python3.5 test2.py data_set/BP_wh_70 6
-echo "--------------------"
-echo "--------------------"
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_80 4
-python3.5 test2.py data_set/BP_wh_80 4
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_80 5
-python3.5 test2.py data_set/BP_wh_80 5
-echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_80 6
-python3.5 test2.py data_set/BP_wh_80 6
+python3 EM2.py data_set/BP_wh_40 6
+python3 test2.py data_set/BP_wh_40 6
 echo "--------------------"
 echo "--------------------"
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_90 4
-python3.5 test2.py data_set/BP_wh_90 4
+python3 EM2.py data_set/BP_wh_50 4
+python3 test2.py data_set/BP_wh_50 4
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_90 5
-python3.5 test2.py data_set/BP_wh_90 5
+python3 EM2.py data_set/BP_wh_50 5
+python3 test2.py data_set/BP_wh_50 5
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_90 6
-python3.5 test2.py data_set/BP_wh_00 6
+python3 EM2.py data_set/BP_wh_50 6
+python3 test2.py data_set/BP_wh_50 6
 echo "--------------------"
 echo "--------------------"
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_100 4
-python3.5 test2.py data_set/BP_wh_100 4
+python3 EM2.py data_set/BP_wh_60 4
+python3 test2.py data_set/BP_wh_60 4
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_100 5
-python3.5 test2.py data_set/BP_wh_100 5
+python3 EM2.py data_set/BP_wh_60 5
+python3 test2.py data_set/BP_wh_60 5
 echo "--------------------"
-python3.5 EM2.py data_set/BP_wh_100 6
-python3.5 test2.py data_set/BP_wh_100 6
+python3 EM2.py data_set/BP_wh_60 6
+python3 test2.py data_set/BP_wh_60 6
+echo "--------------------"
+echo "--------------------"
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_70 4
+python3 test2.py data_set/BP_wh_70 4
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_70 5
+python3 test2.py data_set/BP_wh_70 5
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_70 6
+python3 test2.py data_set/BP_wh_70 6
+echo "--------------------"
+echo "--------------------"
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_80 4
+python3 test2.py data_set/BP_wh_80 4
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_80 5
+python3 test2.py data_set/BP_wh_80 5
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_80 6
+python3 test2.py data_set/BP_wh_80 6
+echo "--------------------"
+echo "--------------------"
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_90 4
+python3 test2.py data_set/BP_wh_90 4
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_90 5
+python3 test2.py data_set/BP_wh_90 5
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_90 6
+python3 test2.py data_set/BP_wh_00 6
+echo "--------------------"
+echo "--------------------"
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_100 4
+python3 test2.py data_set/BP_wh_100 4
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_100 5
+python3 test2.py data_set/BP_wh_100 5
+echo "--------------------"
+python3 EM2.py data_set/BP_wh_100 6
+python3 test2.py data_set/BP_wh_100 6
 echo "--------------------"
 echo "--------------------"
 echo "--------------------"
