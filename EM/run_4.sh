69e41c69aa753cab2a66506b43c24832b0010f13
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-06-29 22:29:41 +0530

    result

--- EM/run_4.sh
+++ EM/run_4.sh
@@ -15,9 +15,9 @@ source ~/keras/bin/activate
 # python3 EM3.py data_set/BP_15_15_40_13 8
 # python3 test4.py data_set/BP_15_15_40_13 8
 # echo "--------------------"
-python3 EM3.py data_set/BP_15_15_40_13 9
-python3 test4.py data_set/BP_15_15_40_13 9
-echo "--------------------"
+# python3 EM3.py data_set/BP_15_15_40_13 9
+# python3 test4.py data_set/BP_15_15_40_13 9
+# echo "--------------------"
 echo "--------------------"
 echo "--------------------"
 # python3 EM3.py data_set/BP_15_15_40_26 7
