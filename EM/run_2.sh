678318ee7bdf180c4edfa8388380b6a523317569
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-07-12 23:28:02 +0530

    result

--- EM/run_2.sh
+++ EM/run_2.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
-#SBATCH -A Research
+#SBATCH -A MLL
 #SBATCH --cpus-per-gpu=2
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=80G
 #SBATCH -t 4-00:00:00
-#SBATCH --output=result3.txt
+#SBATCH --output=result4.txt
 module add cuda/11.6
 module add cudnn/8.4.0-cuda-11.6
 source ~/keras/bin/activate
 
-python3.7 EM2.py data_set/BP_15_15_35_15 7
-python3.7 test3.py data_set/BP_15_15_35_15 7
-echo "--------------------"
-python3.7 EM2.py data_set/BP_15_15_35_15 8
-python3.7 test3.py data_set/BP_15_15_35_15 8
-echo "--------------------"
-python3.7 EM2.py data_set/BP_15_15_35_15 9
-python3.7 test3.py data_set/BP_15_15_35_15 9
-echo "--------------------"
-echo "--------------------"
-echo "--------------------"
-python3.7 EM2.py data_set/BP_15_15_35_20 7
-python3.7 test3.py data_set/BP_15_15_35_20 7
-echo "--------------------"
-python3.7 EM2.py data_set/BP_15_15_35_20 8
-python3.7 test3.py data_set/BP_15_15_35_20 8
-echo "--------------------"
-python3.7 EM2.py data_set/BP_15_15_35_20 9
-python3.7 test3.py data_set/BP_15_15_35_20 9
-echo "--------------------"
-echo "--------------------"
-echo "--------------------"
-python3.7 EM2.py data_set/BP_15_15_35_25 7
-python3.7 test3.py data_set/BP_15_15_35_25 7
+# python3.7 EM2.py data_set/BP_15_15_35_15 7
+# python3.7 test3.py data_set/BP_15_15_35_15 7
+# echo "--------------------"
+# python3.7 EM2.py data_set/BP_15_15_35_15 8
+# python3.7 test3.py data_set/BP_15_15_35_15 8
+# echo "--------------------"
+# python3.7 EM2.py data_set/BP_15_15_35_15 9
+# python3.7 test3.py data_set/BP_15_15_35_15 9
+# echo "--------------------"
+# echo "--------------------"
+# echo "--------------------"
+# python3.7 EM2.py data_set/BP_15_15_35_20 7
+# python3.7 test3.py data_set/BP_15_15_35_20 7
+# echo "--------------------"
+# python3.7 EM2.py data_set/BP_15_15_35_20 8
+# python3.7 test3.py data_set/BP_15_15_35_20 8
+# echo "--------------------"
+# python3.7 EM2.py data_set/BP_15_15_35_20 9
+# python3.7 test3.py data_set/BP_15_15_35_20 9
+# echo "--------------------"
+# echo "--------------------"
+# echo "--------------------"
+# python3.7 EM2.py data_set/BP_15_15_35_25 7
+# python3.7 test3.py data_set/BP_15_15_35_25 7
 echo "--------------------"
 python3.7 EM2.py data_set/BP_15_15_35_25 8
 python3.7 test3.py data_set/BP_15_15_35_25 8
