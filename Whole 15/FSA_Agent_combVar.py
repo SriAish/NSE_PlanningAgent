69a6ffd7fe319a18e037493c996ad7f2e8ab0463
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-07-21 23:12:52 +0530

    result

--- Whole 15/FSA_Agent_combVar.py
+++ Whole 15/FSA_Agent_combVar.py
@@ -9,7 +9,7 @@ from misc import load
 
 class FSAgent:
     def __init__(self, BP, FSA, gamma = 0.999, locations = None):
-        self.m = GEKKO()
+        self.m = GEKKO(remote=False)
         self.m.options.MAX_ITER = 3000
         self.m.options.SOLVER = int(sys.argv[9])
         self.BP = BP
