9b2acaee14d1552bc584b8b039b5f75e451cc0ad
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-Air.bbrouter>
2022-07-25 01:46:15 +0530

    result

---
+++ Navigation/VIAgent.py
@@ -0,0 +1,112 @@
+from misc import NavigationConstants
+import sys
+import pickle
+
+class VIAgent:
+    def __init__(self, BP, gamma = 0.999, delta = 0.1):
+        self.BP = BP
+        g_pos = (3, 6)
+        e_state = []
+        e_state.append(((-1, -1), '?', False, False))
+        self.end_state = e_state
+        g_pos = (3, 6)
+        e_state = []
+        e_state.append((g_pos, 'fast', False, False))
+        e_state.append((g_pos, 'slow', False, False))
+        self.check = e_state
+        self.stateValues = {}
+        self.initializeStateValues()
+        self.gamma = gamma  
+        self.delta = delta
+        self.init_belief()
+
+    def init_belief(self):
+        init_loc = (0, 0)
+        self.belief_state = []
+        self.belief_state.append((init_loc, 'slow', False, False))
+        print(self.belief_state)
+
+    def initializeStateValues(self):
+        for i in self.BP.states:
+            self.stateValues[i] = sys.maxsize
+        for e in self.end_state:
+            self.stateValues[e] = 0
+
+    def update(self):
+        delta = 0
+        for state in self.stateValues:
+            if state in self.end_state:
+                continue
+            actions = self.BP.getValidActions(state)
+            state_cost = sys.maxsize
+            # if state in self.check:
+                # print(state)
+            for action in actions:
+                next_states, state_action_cost = self.BP.transition(state, action)
+                # if state in self.check:
+                #     print(next_states)
+                for ns_prob in next_states:
+                    state_action_cost += self.gamma * ns_prob[1] * self.stateValues[ns_prob[0]]
+                state_cost = min(state_cost, state_action_cost)
+            delta = max(delta, abs(self.stateValues[state] - state_cost))
+            self.stateValues[state] = state_cost
+
+        return delta
+
+    def generatePolicy(self):
+        x = sys.maxsize
+        while x > self.delta:
+            # print(x) 
+            x = self.update()
+        
+        policy = {}
+        for state in self.stateValues:
+            policy[state] = {}
+            if state in self.end_state:
+                policy[state] = []
+                continue
+            actions = self.BP.getValidActions(state)
+            cost = sys.maxsize
+            for i in actions:
+                policy[state][i] = 0
+                next_states, c = self.BP.transition(state, i)
+                for j in next_states:
+                    c += self.gamma * j[1] * self.stateValues[j[0]]
+                if c < cost:
+                    a = i
+                cost = min(cost, c)
+            self.stateValues[state] = cost
+            # print(state, a, cost)
+            policy[state][a] = 1
+        print(self.stateValues[self.belief_state[0]])
+        return policy
+
+class VIPolicy:
+    def __init__(self, name):
+        self.loadPolicy(name)
+
+    def loadPolicy(self, name):
+        file_to_read = open(name, "rb")
+        self.policy = pickle.load(file_to_read)
+
+    def getAction(self, state):
+        return self.policy[(tuple(state[0]), tuple(state[1]), state[2], state[3])]
+
+if __name__ == '__main__':
+    ped = [(0, 1), (2, 3), (5, 6)]
+    pud = [(2, 3), (4, 5), (6, 3)]
+    g_pos = (3, 6)
+    e_state = []
+    e_state.append((g_pos, 'fast', False, False))
+    e_state.append((g_pos, 'slow', False, False))
+    print(e_state)
+    BP = NavigationConstants(int(sys.argv[1]), ped, pud, e_state)
+    agent = VIAgent(BP, delta=0.001)
+    policy = agent.generatePolicy()
+    # print(policy)
+    with open('policy_values/'+ sys.argv[2] + "_" + sys.argv[10] + sys.argv[11] + "_" + sys.argv[6] + sys.argv[7] + '.pkl', 'wb') as f:
+        pickle.dump(policy, f)
+    print("done")
+    # # print(agent.stateValues)
+    # with open('policy_values/'+ sys.argv[9] + '.pkl', 'wb') as f:
+    #     pickle.dump(agent.stateValues, f, pickle.HIGHEST_PROTOCOL)
