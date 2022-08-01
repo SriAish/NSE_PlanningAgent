7225a23ad3323b1855c1e291188bfc2013378796
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-08-01 21:51:24 +0530

    result

--- Whole 15/run.sh
+++ Whole 15/run.sh
@@ -4,33 +4,33 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=80G
 #SBATCH -t 2-00:00:00
-#SBATCH --output=result3.txt
+#SBATCH --output=result5.txt
 module add cuda/11.6
 module add cudnn/8.4.0-cuda-11.6
 source ~/keras/bin/activate
 
 start=`date +%s`
 echo "Start learning FSA"
-python3.7 EM.py data_set/BP_15_15_35_25 3
-echo "Checking Accuracy of learned FSAs"
-python3.7 test.py data_set/BP_15_15_35_25 3
-echo "Start learning FSA"
-python3.7 EM.py data_set/BP_15_15_35_25 4
-echo "Checking Accuracy of learned FSAs"
-python3.7 test.py data_set/BP_15_15_35_25 4
-echo "Start learning FSA"
-python3.7 EM.py data_set/BP_15_15_35_25 5
-echo "Checking Accuracy of learned FSAs"
-python3.7 test.py data_set/BP_15_15_35_25 5
-echo "Start learning FSA"
-python3.7 EM.py data_set/BP_15_15_35_25 6
-echo "Checking Accuracy of learned FSAs"
-python3.7 test.py data_set/BP_15_15_35_25 6
-echo "Start learning FSA"
-python3.7 EM.py data_set/BP_15_15_35_25 7
-echo "Checking Accuracy of learned FSAs"
-python3.7 test.py data_set/BP_15_15_35_25 7
-echo "Start learning FSA"
+# python3.7 EM.py data_set/BP_15_15_35_25 3
+# echo "Checking Accuracy of learned FSAs"
+# python3.7 test.py data_set/BP_15_15_35_25 3
+# echo "Start learning FSA"
+# python3.7 EM.py data_set/BP_15_15_35_25 4
+# echo "Checking Accuracy of learned FSAs"
+# python3.7 test.py data_set/BP_15_15_35_25 4
+# echo "Start learning FSA"
+# python3.7 EM.py data_set/BP_15_15_35_25 5
+# echo "Checking Accuracy of learned FSAs"
+# python3.7 test.py data_set/BP_15_15_35_25 5
+# echo "Start learning FSA"
+# python3.7 EM.py data_set/BP_15_15_35_25 6
+# echo "Checking Accuracy of learned FSAs"
+# python3.7 test.py data_set/BP_15_15_35_25 6
+# echo "Start learning FSA"
+# python3.7 EM.py data_set/BP_15_15_35_25 7
+# echo "Checking Accuracy of learned FSAs"
+# python3.7 test.py data_set/BP_15_15_35_25 7
+# echo "Start learning FSA"
 python3.7 EM.py data_set/BP_15_15_35_25 8
 echo "Checking Accuracy of learned FSAs"
 python3.7 test.py data_set/BP_15_15_35_25 8
---
+++ plot/plot.py
@@ -0,0 +1,24 @@
+import pickle
+import matplotlib.pyplot as plt
+
+states = [5, 6, 7, 8, 9, 10, 11]
+# print(y)
+time = [545.37, 765.48, 3865.61, 3248.19, 6312.34, 28670.02, 52689.99]
+time = [t/60 for t in time]
+
+best_acc = [0.521, 0.551, 0.784, 0.872, 0.878, 0.868, 0.922]
+best_acc = [b*100 for b in best_acc]
+
+# print(y)
+# plotting the points 
+plt.plot(states, time)
+  
+# naming the x axis
+plt.xlabel('Number of states')
+# naming the y axis
+plt.ylabel('Training time (in mins)')
+  
+# function to show the plot
+plt.show()
+
+
