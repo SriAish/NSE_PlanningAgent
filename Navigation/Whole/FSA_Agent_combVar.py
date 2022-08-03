2749bf7e216b04561679021e5a1dfa419151b9df
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-08-03 23:37:36 +0530

    result

--- Navigation/Whole/FSA_Agent_combVar.py
+++ Navigation/Whole/FSA_Agent_combVar.py
@@ -177,14 +177,14 @@ class FSAgent:
         # self.set_bound()
         # print("setting bound")
         # sys.stdout.flush()
-        # if float(sys.argv[6]) >= 0:
-        #     self.make_constraints_eqn3()
-        #     print("eq3")
-        #     sys.stdout.flush()
-        # if float(sys.argv[7]) >= 0:
-        #     self.make_constraints_eqn4()
-        #     print("eq4")
-        #     sys.stdout.flush()
+        if float(sys.argv[6]) >= 0:
+            self.make_constraints_eqn3()
+            print("eq3")
+            sys.stdout.flush()
+        if float(sys.argv[7]) >= 0:
+            self.make_constraints_eqn4()
+            print("eq4")
+            sys.stdout.flush()
 
     def calculate_pi(self):
         self.pi_ = {}
@@ -230,9 +230,9 @@ if __name__ == '__main__':
     print(b_obj_val+((int(sys.argv[6])/100)*b_obj_val))
     g_pos = (int(sys.argv[2]), int(sys.argv[3]))
     g_state = [(g_pos, 'fast', False, False), (g_pos, 'slow', False, False)]
-    # ped = [(0, 1), (1, 1)]
-    # pud = [(0, 2), (1, 2), (2, 1)]
-    BP = NavigationConstants(int(sys.argv[1]), [], [], g_state)
+    ped = [(0, 12),(0, 13),(0, 14),(4, 0),(4, 1),(4, 5),(4, 6),(4, 7),(4, 8),(5, 9),(5, 10),(7, 0),(7, 1),(7, 10),(7, 11),(10, 0),(10, 1),(10, 2),(10, 3),(10, 9),(10, 10),(10, 11),(10, 12),(10, 13),(10, 14),(11, 0),(11, 1),(11, 2),(11, 3),(11, 5),(11, 6),(11, 7),(11, 8),(11, 9),(11, 10),(11, 11),(11, 12),(11, 13),(11, 14)]
+    pud = [(0, 3),(0, 4),(0, 5),(0, 6),(4, 2),(4, 3),(4, 4),(4, 9),(5, 0),(5, 1),(5, 2),(7, 2),(7, 7),(7, 8),(7, 9),(11, 4)]
+    BP = NavigationConstants(int(sys.argv[1]), ped, pud, g_state)
     file_name = sys.argv[4][sys.argv[4].index("/")+1:]
     # file_name = sys.argv[12][sys.argv[12].index("/")+1:]
     delta = load("results/delta/new_" + file_name + "_" + sys.argv[5] + "_best")
