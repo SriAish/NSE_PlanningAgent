8dce3f55c80d33ce1e82158173e05fcd7008ef7a
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-08-04 00:09:52 +0530

    result

---
+++ Navigation/EM/run_3.sh
@@ -0,0 +1,8 @@
+python3.7 EM2.py data/Nav_15_15_25_25 4
+python3.7 test3.py data/Nav_15_15_25_25 4
+echo "--------------------"
+python3.7 EM2.py data/Nav_15_15_25_25 5
+python3.7 test3.py data/Nav_15_15_25_25 5
+echo "--------------------"
+python3.7 EM2.py data/Nav_15_15_25_25 6
+python3.7 test3.py data/Nav_15_15_25_25 6
