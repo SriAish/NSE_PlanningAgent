a1a41406feb04f1e241b30e5bc126cd093e682cf
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-08-04 23:36:23 +0530

    result

--- Navigation/EM/run_2.sh
+++ Navigation/EM/run_2.sh
@@ -4,49 +4,49 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=80G
 #SBATCH -t 4-00:00:00
-#SBATCH --output=result3.txt
+#SBATCH --output=result2.txt
 module add cuda/11.6
 module add cudnn/8.4.0-cuda-11.6
 source ~/keras/bin/activate
 
-python3.7 EM2.py data/Nav_15_15_25_40 4
-python3.7 test3.py data/Nav_15_15_25_40 4
+python3.7 EM2.py data/Nav_15_15_25_40 7
+python3.7 test3.py data/Nav_15_15_25_40 7
 echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_40 5
-python3.7 test3.py data/Nav_15_15_25_40 5
+python3.7 EM2.py data/Nav_15_15_25_40 8
+python3.7 test3.py data/Nav_15_15_25_40 8
 echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_40 6
-python3.7 test3.py data/Nav_15_15_25_40 6
-echo "--------------------"
-echo "--------------------"
-echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_45 4
-python3.7 test3.py data/Nav_15_15_25_45 4
-echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_45 5
-python3.7 test3.py data/Nav_15_15_25_45 5
-echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_45 6
-python3.7 test3.py data/Nav_15_15_25_45 6
+python3.7 EM2.py data/Nav_15_15_25_40 9
+python3.7 test3.py data/Nav_15_15_25_40 9
 echo "--------------------"
 echo "--------------------"
 echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_50 4
-python3.7 test3.py data/Nav_15_15_25_50 4
-echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_50 5
-python3.7 test3.py data/Nav_15_15_25_50 5
-echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_50 6
-python3.7 test3.py data/Nav_15_15_25_50 6
-echo "--------------------"
-echo "--------------------"
-echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_55 4
-python3.7 test3.py data/Nav_15_15_25_55 4
-echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_55 5
-python3.7 test3.py data/Nav_15_15_25_55 5
-echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_25_55 6
-python3.7 test3.py data/Nav_15_15_25_55 6
+# python3.7 EM2.py data/Nav_15_15_25_45 4
+# python3.7 test3.py data/Nav_15_15_25_45 4
+# echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_25_45 5
+# python3.7 test3.py data/Nav_15_15_25_45 5
+# echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_25_45 6
+# python3.7 test3.py data/Nav_15_15_25_45 6
+# echo "--------------------"
+# echo "--------------------"
+# echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_25_50 4
+# python3.7 test3.py data/Nav_15_15_25_50 4
+# echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_25_50 5
+# python3.7 test3.py data/Nav_15_15_25_50 5
+# echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_25_50 6
+# python3.7 test3.py data/Nav_15_15_25_50 6
+# echo "--------------------"
+# echo "--------------------"
+# echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_25_55 4
+# python3.7 test3.py data/Nav_15_15_25_55 4
+# echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_25_55 5
+# python3.7 test3.py data/Nav_15_15_25_55 5
+# echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_25_55 6
+# python3.7 test3.py data/Nav_15_15_25_55 6
