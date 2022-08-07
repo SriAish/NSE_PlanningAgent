1f7b7670efdf265f8af4b2bd832deaed55d480a4
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-08-08 00:03:17 +0530

    result

--- Whole 15/run3.sh
+++ Whole 15/run3.sh
@@ -9,21 +9,13 @@ module add cuda/11.6
 module add cudnn/8.4.0-cuda-11.6
 source ~/keras/bin/activate
 
-start=`date +%s`
-echo "running agent"
-python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_6_15_00 1 0 0 data_set/BP_15_15_35_25 6 15 > LP_sm_1_15_1_2_6_0_0.txt
-end=`date +%s`
-runtime=$((end-start))
-echo $runtime
-echo "--------------------"
 # start=`date +%s`
 # echo "running agent"
-# python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_7_15_00 1 0 0 data_set/BP_15_15_35_25 7 15 > LP_sm_1_15_1_2_7_0_0.txt
+# python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_6_15_00 1 0 0 data_set/BP_15_15_35_25 6 15 > LP_sm_1_15_1_2_6_0_0.txt
 # end=`date +%s`
 # runtime=$((end-start))
 # echo $runtime
-# start=`date +%s`
-echo "--------------------"
+# echo "--------------------"
 python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_8_15_00 1 0 0 data_set/BP_15_15_35_25 8 15 > LP_sm_1_15_1_2_8_0_0.txt
 end=`date +%s`
 runtime=$((end-start))
@@ -89,7 +81,6 @@ end=`date +%s`
 runtime=$((end-start))
 echo $runtime
 echo "--------------------"
-
 # start=`date +%s`
 # echo "running agent"
 # python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_5_15_00 1 0 0 data_set/BP_15_15_35_25 5 15 > LP_sm_1_15_1_2_5_0_0.txt
@@ -170,6 +161,90 @@ echo "--------------------"
 start=`date +%s`
 echo "running agent"
 python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_4_15_23 1 0.2 0.3 data_set/BP_15_15_35_25 4 15 > LP_sm_1_15_1_2_4_2_3.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+echo "--------------------"
+echo "--------------------"
+echo "--------------------"
+echo "--------------------"
+start=`date +%s`
+echo "running agent"
+python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_6_15_00 1 0 0 data_set/BP_15_15_35_25 6 0 > LP_sm_1_0_1_2_6_0_0.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+echo "running agent"
+python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_7_15_00 1 0 0 data_set/BP_15_15_35_25 7 0 > LP_sm_1_0_1_2_7_0_0.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+start=`date +%s`
+echo "--------------------"
+python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_8_15_00 1 0 0 data_set/BP_15_15_35_25 8 0 > LP_sm_1_0_1_2_8_0_0.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+# start=`date +%s`
+# echo "running agent"
+# python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_6_15_00 1 0 0 data_set/BP_15_15_35_25 6 15 > LP_sm_1_15_1_2_6_0_0.txt
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
+# echo "--------------------"
+start=`date +%s`
+echo "running agent"
+python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_7_15_00 1 0 0 data_set/BP_15_15_35_25 7 15 > LP_sm_1_15_1_2_7_0_0.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+# start=`date +%s`
+# echo "--------------------"
+# python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_8_15_00 1 0 0 data_set/BP_15_15_35_25 8 15 > LP_sm_1_15_1_2_8_0_0.txt
+# end=`date +%s`
+# runtime=$((end-start))
+# echo $runtime
+echo "--------------------"
+start=`date +%s`
+echo "running agent"
+python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_6_15_00 1 0 0 data_set/BP_15_15_35_25 6 20 > LP_sm_1_20_1_2_6_0_0.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+echo "running agent"
+python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_7_15_00 1 0 0 data_set/BP_15_15_35_25 7 20 > LP_sm_1_20_1_2_7_0_0.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+start=`date +%s`
+echo "--------------------"
+python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_8_15_00 1 0 0 data_set/BP_15_15_35_25 8 20 > LP_sm_1_20_1_2_8_0_0.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+echo "running agent"
+python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_6_15_00 1 0 0 data_set/BP_15_15_35_25 6 25 > LP_sm_1_25_1_2_6_0_0.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+echo "--------------------"
+start=`date +%s`
+echo "running agent"
+python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_7_15_00 1 0 0 data_set/BP_15_15_35_25 7 25 > LP_sm_1_25_1_2_7_0_0.txt
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
+start=`date +%s`
+echo "--------------------"
+python3.7 FSA_Agent_combVar.py 15 7 3 6 4 7 14 15_15_0_1_8_15_00 1 0 0 data_set/BP_15_15_35_25 8 25 > LP_sm_1_25_1_2_8_0_0.txt
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
