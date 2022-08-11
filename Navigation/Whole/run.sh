3e9bb81e89a473449a5f0c003917c3aa1cebabda
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-08-11 11:00:39 +0530

    result

--- Navigation/Whole/run.sh
+++ Navigation/Whole/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -A MLL
+#SBATCH -A Research
 #SBATCH --cpus-per-gpu=2
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --mem-per-cpu=80G
 #SBATCH -t 2-00:00:00
 #SBATCH --output=result.txt
