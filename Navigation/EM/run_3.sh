ce2bb7f9661badfdfccc1ae71028c3961dc0261d
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2022-08-13 11:43:52 +0530

    result

--- Navigation/EM/run_3.sh
+++ Navigation/EM/run_3.sh
@@ -9,12 +9,14 @@ module add cuda/11.6
 module add cudnn/8.4.0-cuda-11.6
 source ~/keras/bin/activate
 
-python3.7 EM2.py data/Nav_15_15_30_300 4
-python3.7 test3.py data/Nav_15_15_30_300 4
-echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_30_300 7
-python3.7 test3.py data/Nav_15_15_30_300 7
-echo "--------------------"
-python3.7 EM2.py data/Nav_15_15_30_300 8
-python3.7 test3.py data/Nav_15_15_30_300 8
-echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_30_300 4
+# python3.7 test3.py data/Nav_15_15_30_300 4
+# echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_30_300 7
+# python3.7 test3.py data/Nav_15_15_30_300 7
+# echo "--------------------"
+# python3.7 EM2.py data/Nav_15_15_30_300 8
+# python3.7 test3.py data/Nav_15_15_30_300 8
+# echo "--------------------"
+python3.7 EM.py data/Nav_15_15_30_300_2 5
+python3.7 test3.py data/Nav_15_15_30_300 5
